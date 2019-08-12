57873540f5c024416940e05c45cc17e0a19cbd8d
Jacob Bashista <jacob@orange.haus>
2019-08-12 10:06:11 -0400

    Update chat.py
    Added prompts
    Changed while true to while not quit

--- simplechat/chat.py
+++ simplechat/chat.py
@@ -9,6 +9,12 @@ def processInput(userInput):
         "used": "Cattle are commonly raised as livestock for meat (beef or veal, see beef cattle), for milk (see dairy cattle), and for hides, which are used to make leather."
     }
 
+    prompts = {
+        "what": "What are cows?",
+        "use": "What use is there for cows?",
+        "used": "What are cows used for?"
+    }
+
     userInput = re.sub(r'[^\w\s]','', userInput)
 
     words = userInput.split(" ")
@@ -24,29 +30,31 @@ def processInput(userInput):
     elif len(matchingKeys) <= 1:
         return responses[matchingKeys[0]]
     else:
-        print("What would you like to know more about?")
+        print("I am not quite sure about what you mean. Did you mean: ")
+        index = 1
         for key in matchingKeys:
-            print(key + " ")
+            print(str(index) + ": " + prompts[key] + " ")
+            index += 1
         valid = False
         key = ""
         while not valid:
-            key = input("?: ").lower()
-            if key in matchingKeys:
+            selected = int(input("#: ").lower())
+            if selected <= len(matchingKeys) and selected > 0:
                 break
             else:
                 print("Please enter one of the above")
-        return responses[key]
+        return responses[matchingKeys[selected - 1]]
 
 def main():
     print("Welcome to Cow Facts! I can talk to you about cows!\n")
     print("Ask me a question or type quit\n")
 
-    while True:
+    userInput = ""
+
+    while userInput != "quit":
         userInput = input("Whats your question? ").lower()
 
-        if (userInput == "quit"):
-            break
-        else:
+        if userInput != "quit":
             response = processInput(userInput)
             print(response)
 
