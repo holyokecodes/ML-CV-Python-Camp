b88ad7edbc078b2a44ebfe8adeafc04ccf4e2b2a
Andrew Pasquale <andrew@elytra.net>
2019-08-20 11:13:47 -0400

    Updates to OpenCV face tracking game

--- face-trackinggame/facegame.py
+++ face-trackinggame/facegame.py
@@ -1,23 +1,26 @@
+# In this game, try to get the target boxes 
+# with the box drawn around your head.
 import cv2
 import time
-# Will need random to choose our box positions
 import random
 
+CAPTURE_WIDTH = 1280
+CAPTURE_HEIGHT = 720
 cap = cv2.VideoCapture(0)
-cap.set(3, 320)
-cap.set(4, 240)
+cap.set(3, CAPTURE_WIDTH)
+cap.set(4, CAPTURE_HEIGHT)
 
 classifer = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 time.sleep(1)
 
-# We need to keep track of our points
+# Keep track of our points
 points = 0
 # If we have hit the target, defaulting to true
 hitTarget = True
-# And the targets x, y and width
+# target x and y position
 tX = 0
 tY = 0
-# Set the width to 20 for now, we can change this to best suit your game
+# width of the target box
 tW = 20
 
 while(True):
@@ -30,17 +33,17 @@ while(True):
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
         # Check if our target square is within the bounds of a detected face
-        if (tX >= x and tX <= x + w) and (tY >= y and tY <= y + h):
+        if (x <= tX <= x + w) and (y <= tY <= y + h):
             # If so, flag that we have hit the target and add a point
             hitTarget = True
             points += 1
 
     # Check if we have hit the target, will also run the first time our game opens
     if hitTarget:
-        # Choose a new x position, the 640 is our width * 2, and we subtract the width so it doesn't appear offscreen
-        tX = random.randint(tW, 320 - tW)
-        # Same goes with y but the 480 is our width * 2
-        tY = random.randint(tW, 240 - tW)
+        # Choose a new x position, and subtract the target width so it doesn't appear offscreen
+        tX = random.randint(tW, CAPTURE_WIDTH - tW)
+        # Same goes with y
+        tY = random.randint(tW, CAPTURE_HEIGHT - tW)
         # And reset if we have hit the target
         hitTarget = False
 
@@ -52,6 +55,7 @@ while(True):
 
     cv2.imshow('Face Tracking', img)
     
+    # Close the OpenCV window if you press 'q' key
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
